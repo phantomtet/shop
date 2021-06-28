621c0b146ac45aba37bfce500fe94608280a5dde
phantomtet <phantomtet16698@gmail.com>
2021-06-28 19:02:49 +0700

    update login UI

--- src/components/Login2.js
+++ src/components/Login2.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './Login.css'
 import { firestore, auth } from '../firebase'
 import { useFirebase } from 'react-redux-firebase'
 import {FcGoogle,} from 'react-icons/fc'
@@ -28,9 +28,46 @@ export default function Login2 () {
         })
     }
     return (
-        <div id='login' className='test' style={{position: 'fixed', height: '100%', width: '100%',}}>
-            <div style={{ background: 'linear-gradient(132deg, #FC415A, #591BC5, #212335)', animation: 'Gradient 15s ease infinite', backgroundSize: '400% 400%', height: '100vh', width: '100%'}}>
-                a
+        <div id='login'  style={{display: 'flex', justifyContent: 'center', position: 'fixed', height: '100%', width: '100%', background: 'linear-gradient(to top, #003b46, #00ccff)'}}>
+            <div className='test' style={{ width: '100%', maxWidth: '400px', height: '100%', maxHeight: '600px', margin: 'auto'}}>
+                {/* tieu de */}
+                <div style={{display: 'flex', fontSize: '30px', width: '100%', justifyContent: 'center',}}>
+                    Fakebook
+                </div>
+                <div style={{ padding: '10px', marginTop: '10px'}}>
+                    <div style={{fontSize: '20px', display: 'flex', justifyContent: 'center'}}>
+                        Have an account?
+                    </div>
+                    <input placeholder='Username' style={{width: '100%', margin: '15px 0 10px', fontSize: '25px', height: '40px', border: 'none', borderRadius: '30px', backgroundColor: 'rgba(0, 0, 0, 0.25)'}}/>
+                    <input type='password' placeholder='Password' style={{width: '100%', marginBottom: '15px', fontSize: '25px', height: '40px', border: 'none', borderRadius: '30px', backgroundColor: 'rgba(0, 0, 0, 0.25)'}}/>
+                    <div id='signin'>
+                        SIGN IN
+                    </div>
+                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <div style={{display: 'flex', paddingTop: '10px'}}>
+                            <input type='checkbox' style={{margin: 'auto 5px'}}/>
+                            <p className='canclick2'>Remember me</p>
+                        </div>
+                        <div className='canclick2 ' style={{marginTop: '10px'}}>
+                            Forgot password
+                        </div>
+                    </div>
+                </div>
+                <div style={{marginTop: '10px'}}>
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                        Or Sign In With
+                    </div>
+                    <div style={{display: 'flex', }}>
+                        <div className='login-button'>
+                            <FcGoogle style={{margin: 'auto 5px auto auto'}}/>
+                            <p style={{margin: 'auto auto auto 0'}}>Google</p>
+                        </div>
+                        <div className='login-button'>
+                            <FaUserTie style={{margin: 'auto 5px auto auto'}}/>
+                            <p style={{margin: 'auto auto auto 0'}}>Guest Account</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
